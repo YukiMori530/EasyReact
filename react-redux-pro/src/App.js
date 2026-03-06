a5b9ba31f04659f51a5fe00a21884c7fcb409051
liuzixi <alexanderseabra123@gmail.com>
2026-03-06 09:21:07 +0800

    Redux与React实现counter

---
+++ react-redux-pro/src/App.js
@@ -0,0 +1,15 @@
+import {useDispatch, useSelector} from 'react-redux';
+import {decrement, increment} from './store/modules/counterStore';
+function App() {
+  const {count} = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  return (
+    <div className="App">
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch(increment())}>+</button>
+    </div>
+  );
+}
+
+export default App;
